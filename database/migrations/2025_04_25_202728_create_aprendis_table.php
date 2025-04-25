4f074d93d182e15c8ccd9a6ef73eafb41b97273d
Ovirne <balantaovirne@gmail.com>
2025-04-25 15:37:20 -0500

    comit admin sena en laravel

---
+++ database/migrations/2025_04_25_202728_create_aprendis_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('aprendis', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->integer('cell_number');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('aprendis');
+    }
+};
